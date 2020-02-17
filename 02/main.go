61458c52c1dd6c5daffa5b68d6871784127abcdb
Jérémy Goupil <goupiljeremy@gmail.com>
2020-02-17 15:54:49 +0100

    generate html via powershell and main.go

--- 02/main.go
+++ 02/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	name := "Todd McLeod"
-
-	tpl := `
+	name := os.Args[1]
+	fmt.Println(os.Args[0])
+	fmt.Println(os.Args[1])
+	str := fmt.Sprint(`
 	<!DOCTYPE html>
 	<html lang="en">
 	<head>
@@ -15,7 +22,15 @@ func main() {
 	<body>
 	<h1>` + name + `</h1>
 	</body>
-	</html>`
+	</html>`)
 
-	fmt.Println(tpl)
+
+
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal("error creating file", err)
+	}
+	defer nf.Close()
+
+	io.Copy(nf, strings.NewReader(str))
 }
